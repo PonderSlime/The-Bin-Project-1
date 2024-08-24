863eab47ed7fbe16c208bf58e7364430a81eea7c
PonderSlime <micah.j.edwards1@gmail.com>
2024-08-24 17:35:46 -0600

    Encoder wired and working

--- files/main.py
+++ files/main.py
@@ -74,16 +74,16 @@ scroll_ammt = 32
 # np.write()
 # 
 # 
-# SW = Pin(22,Pin.IN,Pin.PULL_UP)  
-# 
-# r = RotaryIRQ(pin_num_clk=20,
-#               pin_num_dt=17,
-#               min_val=0,
-#               reverse=False,
-#               range_mode=RotaryIRQ.RANGE_UNBOUNDED)
-# val_old = r.value()
-# 
-# 
+SW = Pin(22,Pin.IN,Pin.PULL_UP)  
+ 
+r = RotaryIRQ(pin_num_clk=20,
+              pin_num_dt=17,
+              min_val=0,
+              reverse=False,
+              range_mode=RotaryIRQ.RANGE_UNBOUNDED)
+val_old = r.value()
+
+
 # #c_note()
 # #main()
 xAxis = ADC(Pin(27))
@@ -127,7 +127,7 @@ def joy():
         #np.write()
         #d_note()
 
-    print("X: " + xStatus + ", Y: " + yStatus + " -- button " + buttonStatus)
+    #print("X: " + xStatus + ", Y: " + yStatus + " -- button " + buttonStatus)
 #
 oled.fill(0)
 oled.text("Hello, World!", 10, 10)
@@ -188,26 +188,26 @@ while True:
     else:
         scroll_ammt = 32
            
-#   try:  
-#     val_new = r.value()  
-#     if SW.value()==0 and n==0:  
-#         print("Button Pressed")  
-#         print("Selected Number is : ",val_new) 
-#         #c_note()
-#         np[0] = (255, 255, 0) # set to yellow, full brightness
-#         np.write()
-#         n=1 
-#         while SW.value()==0:  
-#             continue  
-#     n=0  
-#     if val_old != val_new:  
-#         val_old = val_new  
-#         print('result =', val_new)
-#         #c_note()
-#         np[0] = (0, 255, 255) # set to teal, full brightness
-#         np.write()
-#   
-#   except KeyboardInterrupt:  
-#     break
-#   #time.sleep_ms(50)'''
+    try:  
+        val_new = r.value()  
+        if SW.value()==0 and n==0:  
+            print("Button Pressed")  
+            print("Selected Number is : ",val_new) 
+            #c_note()
+            #np[0] = (255, 255, 0) # set to yellow, full brightness
+            #np.write()
+            n=1 
+            #while SW.value()==0:  
+                #continue  
+        n=0  
+        if val_old != val_new:  
+            val_old = val_new  
+            print('result =', val_new)
+            #c_note()
+            #np[0] = (0, 255, 255) # set to teal, full brightness
+            #np.write()
+  
+    except KeyboardInterrupt:  
+        break
+    #time.sleep_ms(50)
 
